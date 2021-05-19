a709f5d38c6760ff7c2bea5a2cd0f04485ba2bdb
Simon Jacobsen <simon.jac@hotmail.com>
2021-05-19 19:04:53 +0200

    Added Player Prefab with FP Movement
    works with keyboard and controller
    ajustable variables for mov.speed and sensistivity x & y

---
+++ Assets/Player/InputManager.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputManager : MonoBehaviour
+{
+#pragma warning disable 649
+
+    [SerializeField] Movement movement;
+    [SerializeField] PlayerLook playerLook;
+
+    PlayerControls controls;
+    PlayerControls.GroundMovementActions groundMove;
+
+    Vector2 horizontalInput;
+    Vector3 mouseInput;
+
+    private void Awake()
+    {
+        controls = new PlayerControls();
+        groundMove = controls.GroundMovement;
+
+        groundMove.HorizontalMovement.performed += ctx => horizontalInput = ctx.ReadValue<Vector2>();
+
+        groundMove.MouseX.performed += ctx => mouseInput.x = ctx.ReadValue<float>();
+        groundMove.MouseY.performed += ctx => mouseInput.y = ctx.ReadValue<float>();
+    }
+
+    private void Update()
+    {
+        movement.RecieveInput(horizontalInput);
+        playerLook.RecieveInput(mouseInput);
+    }
+
+    private void OnEnable()
+    {
+        controls.Enable();
+    }
+
+    private void OnDestroy()
+    {
+        controls.Disable();
+    }
+}
